3e7e83047f33005889bf1384d7531ff945e3e3c7
Talha76 <mushfiqurrahman845@gmail.com>
2023-10-30 00:05:05 +0600

    Added documentation

--- src/models/User.class.ts
+++ src/models/User.class.ts
@@ -2,12 +2,40 @@ import Database from '../config/Database.class.js';
 import Person from './Person.class.js';
 import Vehicle from './Vehicle.class.js';
 
+/**
+ * @class User
+ * @description User class
+ * @extends Person
+ * @param {string} name - Name of the user
+ * @param {string} mail - Mail of the user
+ * @param {string} password - Password of the user
+ * @param {number} id - ID of the user
+ * @param {string} phoneNumber - Phone number of the user
+ * @param {boolean} isStudent - Is the user a student?
+ * @param {Vehicle[]} vehicleList - List of vehicles of the user
+ * @since 1.0.0
+ * @version 1.0.0
+ * @exports User
+ * @see Person
+ * @see Vehicle
+ * @constructor User({ name = null, mail = null, password = null, id = null, phoneNumber = null, isStudent = false, vehicleList = [] })
+ */
 class User extends Person {
   private _id: number;
   private _phoneNumber: string;
   private _isStudent: boolean;
   private _vehicleList: Vehicle[];
 
+  /**
+   * @constructor
+   * @param name
+   * @param mail
+   * @param password
+   * @param id
+   * @param phoneNumber
+   * @param isStudent
+   * @param vehicleList
+   */
   constructor({
     name = null,
     mail = null,
@@ -24,6 +52,17 @@ class User extends Person {
     vehicleList.forEach(vehicle => this._vehicleList.push(new Vehicle(vehicle)));
   }
 
+  /**
+   * @async
+   * @method initialize
+   * @description Initialize the user
+   * @returns {Promise<void>}
+   * @since 1.0.0
+   * @version 1.0.0
+   * @example
+   * const user = new User({ mail: 'example@gmail.com' });
+   * await user.initialize();
+   */
   async initialize() {
     try {
       const sql = `SELECT *
@@ -50,36 +89,73 @@ class User extends Person {
     }
   }
 
+  /**
+   * @method id
+   * @description Get the ID of the user
+   */
   get id(): number {
     return this._id;
   }
 
+  /**
+   * @method phoneNumber
+   * @description Get the phone number of the user
+   */
   get phoneNumber(): string {
     return this._phoneNumber;
   }
 
+  /**
+   * @method isStudent
+   * @description Is the user a student?
+   */
   get isStudent(): boolean {
     return this._isStudent;
   }
 
+  /**
+   * @method vehicleList
+   * @description Get the list of vehicles of the user
+   */
   get vehicleList(): Vehicle[] {
     return this._vehicleList;
   }
 
+  /**
+   * @method addVehicle
+   * @param vehicle {Vehicle} - Vehicle to add
+   * @description Add a vehicle to the user
+   */
   addVehicle(vehicle: Vehicle): void {
     this._vehicleList.push(vehicle);
   }
 
+  /**
+   * @method removeVehicle
+   * @param vehicle {Vehicle} - Vehicle to remove
+   * @description Remove a vehicle from the user
+   */
   removeVehicle(vehicle: Vehicle): void {
     this._vehicleList = this._vehicleList.filter(v => v.licenseNumber !== vehicle.licenseNumber);
   }
 
-  async save(): Promise<void> {
+  /**
+   * @async
+   * @method save
+   * @description Save the user in the database
+   */
+  async save(){
     const db = Database.getInstance();
     const sql = `INSERT INTO "users" VALUES ('${this.id}', ${this.name}', '${this.mail}', '${this.password}', '${this.phoneNumber}', '${this.isStudent}')`;
     await db.query(sql);
   }
 
+  /**
+   * @async
+   * @method changePassword
+   * @param password {string} - New password
+   * @description Change the password of the user
+   */
   async changePassword(password: string) {
     this._password = password;
     const db = Database.getInstance();
