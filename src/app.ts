e5717f246d406fb86a04f9a92402d2ddef06f545
Talha76 <mushfiqurrahman845@gmail.com>
2023-10-29 21:07:24 +0600

    Removed multer

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-const app = express();
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import flash from 'connect-flash';
 import session from 'express-session';
 import passport from 'passport';
+import fileUpload from 'express-fileupload';
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -26,6 +28,8 @@ app.set('view engine', 'ejs');
 app.set('views', './src/views')
 
 app.use(express.static('./src/public'));
+
+app.use(fileUpload());
 
 import indexRoutes from './routes/index.routes.js';
 app.use(indexRoutes);
--- src/routes/admin/admin.routes.ts
+++ src/routes/admin/admin.routes.ts
@@ -1,24 +1,14 @@
 import {
   getAdmin,
   postAdmin,
-  getNumberPlateInfo,
   postSendImage,
 } from '../../controllers/admin/admin.controllers.js';
 import { Router } from 'express';
-import multer from 'multer';
-
-const storage = multer.diskStorage({
-  destination: '../../images/',
-  filename: (req, file, cb) => cb(null, file.originalname)
-});
-
-const upload = multer({ storage: storage });
 
 const route = Router();
 
 route.get('/', getAdmin);
 route.post('/', postAdmin);
-route.get('/get-np-info', getNumberPlateInfo);
-route.post('/send-image', upload.single('image'),  postSendImage);
+route.post('/send-image', postSendImage);
 
 export default route;
