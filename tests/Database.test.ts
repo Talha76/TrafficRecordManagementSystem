86c5ef44b4db0402b0299edc02bb5353f9e7ae59
Talha76 <mushfiqurrahman845@gmail.com>
2023-11-05 21:17:59 +0600

    Updated database test to only test the database

--- tests/Database.test.ts
+++ tests/Database.test.ts
@@ -8,24 +8,10 @@ describe('Database', () => {
     expect(db).toBeInstanceOf(Database);
   });
 
-  it('Should return a valid user', async () => {
-    const mail = 'a@gmail.com';
-    const sql = `SELECT *
-                 FROM user_vehicle_info
-                 WHERE mail = '${mail}'`;
-    const result = await db.query(sql);
-    expect(result).toBeDefined();
-    expect(result).toHaveLength(2);
-  });
-
-  it('Should return a valid user even though a car is not registered to his/her name', async () => {
-    const mail = 'a@mail.co';
-    const sql = `SELECT *
-                 FROM user_vehicle_info
-                 WHERE mail = '${mail}'`;
+  it('Should return a valid response', async () => {
+    const sql = `SELECT NOw()`;
     const result = await db.query(sql);
     expect(result).toBeDefined();
     expect(result).toHaveLength(1);
-    await db.end();
   });
 });
