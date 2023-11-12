cd8797ef8a4794574b4a2b4c002e13409100f8cf
Talha76 <mushfiqurrahman845@gmail.com>
2023-11-13 03:40:34 +0600

    Edited service tests

--- tests/Services.test.js
+++ tests/Services.test.js
@@ -2,8 +2,6 @@ import {describe, it, expect, afterAll} from "@jest/globals";
 import * as User from '../src/services/User.services.js'
 import sequelize from "../src/config/sequelize.config.js";
 
-afterAll(async () => sequelize.close());
-
 describe('User Services Tests', () => {
   it ('Should return a valid user when querying by ID', async () => {
     const user = User.findUserById(200042124);
