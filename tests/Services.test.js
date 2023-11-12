845093336aa334ba859b9858ce40a5c3791d3ed2
Talha76 <mushfiqurrahman845@gmail.com>
2023-11-13 03:38:48 +0600

    Added tests for User service

---
+++ tests/Services.test.js
@@ -0,0 +1,17 @@
+import {describe, it, expect, afterAll} from "@jest/globals";
+import * as User from '../src/services/User.services.js'
+import sequelize from "../src/config/sequelize.config.js";
+
+afterAll(async () => sequelize.close());
+
+describe('User Services Tests', () => {
+  it ('Should return a valid user when querying by ID', async () => {
+    const user = User.findUserById(200042124);
+    expect(user).not.toBeNull();
+  });
+
+  it ('Should return a valid user when querying by email', async () => {
+    const user = User.findUserByEmail('mushfiqurtalha@iut-dhaka.edu');
+    expect(user).not.toBeNull();
+  });
+})
