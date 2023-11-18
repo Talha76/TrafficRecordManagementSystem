48768f366da5e09e882b07e8603d6c6652d3a90d
Ryuzaki-L-07 <shahrier@iut-dhaka.edu>
2023-11-18 17:53:32 +0600

    Front-end updated with license detection

--- src/controllers/user/user.controllers.js
+++ src/controllers/user/user.controllers.js
@@ -5,7 +5,14 @@ const getUserDashboard = async (req, res) => {
   let _mail = req.user.email;
   try {
     const user = await User.findUserByEmail(_mail);
-    const vehicles = await Vehicle.getVehicleList({userMail: _mail});
+    const vehicles = await Vehicle.getVehicleList(
+        {
+          userMail: _mail,
+          approvalStatus: true
+        }
+      );
+
+      console.log(vehicles);
     res.render('user/user.dashboard.ejs', {
       "user": user,
       "vehicles": vehicles
@@ -15,20 +22,50 @@ const getUserDashboard = async (req, res) => {
   }
 }
 
+// const addVehicle = async (req, res) => {
+//   const {licenseNumber, vehicleName} = req.body;
+//   const _mail = req.user.email;
+//   try {
+//     const vehicle = await Vehicle.findVehicleByLicenseNumber(licenseNumber);
+//     if(vehicle != null) {
+//       res.redirect('/dashboard');
+//     }
+
+//     await Vehicle.addVehicle({
+//       userMail: _mail,
+//       licenseNumber: licenseNumber,
+//       vehicleName: vehicleName
+//     });
+//     res.redirect('/dashboard');
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
 const addVehicle = async (req, res) => {
-  const {licenseNumber, vehicleName} = req.body;
+  const { licenseNumber, vehicleName } = req.body;
   const _mail = req.user.email;
   try {
+    const vehicle = await Vehicle.findVehicleByLicenseNumber(licenseNumber);
+    if (vehicle != null) {
+      res.render('user/user.dashboard.ejs', {
+        error: 'Vehicle already exists', // Set an error message
+      });
+      res.redirect('/dashboard');
+      return; // Return to prevent further execution
+    }
+    
     await Vehicle.addVehicle({
       userMail: _mail,
       licenseNumber: licenseNumber,
-      vehicleName: vehicleName
+      vehicleName: vehicleName,
     });
     res.redirect('/dashboard');
   } catch (err) {
     console.error(err);
   }
-}
+};
+
 
 const removeVehicle = async (req, res) => {
   const {licenseNumber} = req.query;
--- src/routes/user/user.routes.js
+++ src/routes/user/user.routes.js
@@ -5,6 +5,6 @@ import userMiddlewares from '../../middlewares/user/user.middlewares.js';
 const router = Router();
 
 router.get('/dashboard', userMiddlewares.isloggedIn, userMiddlewares.emailVerificationMiddleware, userControllers.getUserDashboard);
-router.post('/dashboard', userMiddlewares.isloggedIn, userControllers.addVehicle);
+router.post('/dashboard/addVehicle', userMiddlewares.isloggedIn, userControllers.addVehicle);
 router.get('/dashboard/removeVehicle', userMiddlewares.isloggedIn, userControllers.removeVehicle);
 export default router;
