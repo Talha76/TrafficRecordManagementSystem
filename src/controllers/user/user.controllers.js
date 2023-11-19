9a7a76e682d7d7c1781a4f42d9891e98db0b41cc
Talha76 <mushfiqurrahman845@gmail.com>
2023-11-20 00:37:39 +0600

    Fix vehicle addition and removal routes

--- src/controllers/user/user.controllers.js
+++ src/controllers/user/user.controllers.js
@@ -20,6 +20,21 @@ const addVehicle = async (req, res) => {
   const {licenseNumber, vehicleName} = req.body;
   const _mail = req.user.email;
   try {
+    const vehicles = await Vehicle.getVehicleList({userMail: _mail, approvalStatus: true});
+    if (vehicles.length >= parseInt(process.env.MAX_VEHICLE)) {
+      req.flash('message', `You can't add more than ${process.env.MAX_VEHICLE} vehicles.`);
+      return res.redirect('/dashboard');
+    }
+
+    const vehicle = await Vehicle.getVehicleList({
+      licenseNumber: licenseNumber,
+      approvalStatus: true
+    });
+    if (vehicle) {
+      req.flash('message', `This vehicle is already registered.`);
+      return res.redirect('/dashboard');
+    }
+
     await Vehicle.addVehicle({
       userMail: _mail,
       licenseNumber: licenseNumber,
