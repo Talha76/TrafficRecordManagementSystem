3e6a62984709b8190fb7d2bef629f5e2d369face
Talha76 <mushfiqurrahman845@gmail.com>
2024-01-16 00:01:01 +0600

    Create: Mailer config

---
+++ src/config/mailer.config.js
@@ -0,0 +1,13 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  }
+});
