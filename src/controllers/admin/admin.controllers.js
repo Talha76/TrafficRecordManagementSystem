ee8b96cd4fad583e001fd8573b07b6c467ca7807
Ryuzaki-L-07 <shahrier@iut-dhaka.edu>
2024-01-16 22:47:18 +0600

    Revert "Merge branch 'development' into approval-page"
    This reverts commit dd7783381f4dc5286eba87085cfa1a499de07b9a, reversing
    changes made to 8db4fde2d064414e5a4aa9ab01c17c883832c4a4.

--- src/controllers/admin/admin.controllers.js
+++ src/controllers/admin/admin.controllers.js
@@ -441,7 +441,7 @@ async function getGenerateReport(req, res) {
     const appUser = req.user;
     appUser.designation = appUser.designation === "sco" ? "SCO" : "PT";
     req.flash("appUser", appUser);
-    res.render("./admin/reportGeneration.ejs", {
+    res.render("./admin/reportGEneration.ejs", {
       error: req.flash("error"),
       success: req.flash("success"),
       appUser: req.flash("appUser")[0],
