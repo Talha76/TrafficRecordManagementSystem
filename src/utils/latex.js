ae19b9fdae5849fe5a2a63ef789919ae85d01a0d
Talha76 <mushfiqurrahman845@gmail.com>
2024-01-17 16:26:57 +0600

    Complete: Report Generation Service

--- src/utils/latex.js
+++ src/utils/latex.js
@@ -3,14 +3,14 @@ import {exec} from "child_process";
 
 export default function latexToPdf(data) {
   if (data === undefined || data === null) data = "";
-  const start = fs.readFileSync("start.tex", "utf-8");
-  const end = fs.readFileSync("end.tex", "utf-8");
+  const start = fs.readFileSync("src/report/start.tex", "utf-8");
+  const end = fs.readFileSync("src/report/end.tex", "utf-8");
 
   const finalData = start + data + end;
 
-  fs.writeFileSync("final.tex", finalData);
+  fs.writeFileSync("src/report/final.tex", finalData);
 
-  const cmd = "pdflatex -jobname=output final.tex";
+  const cmd = "xelatex -jobname=output src/report/final.tex";
   const child = exec(cmd);
   child.stdout.on("data", data => console.log(`stdout: ${data}`));
   child.stderr.on("data", data => console.error(`stderr: ${data}`));
